f87587d39e18665dd53db9f9135624fa1ea66baa
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-01-01 22:58:08 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,8 +1,9 @@
 require 'nokogiri'
-
+require 'pry'
 def create_project_hash
   html =  File.read('fixtures/kickstarter.html')
 
   kickstarter = Nokogiri::HTML(html)
   # projects: kickstarter.css("li.project.grid_4")
+  binding.pry
 end
