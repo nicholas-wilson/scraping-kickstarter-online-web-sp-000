4b6326c0637538a88ec71edd35cb437d951c4e96
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-01-02 00:10:23 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -19,4 +19,5 @@ def create_project_hash
       :location => project.css("ul.project-meta li a span.location-name").text,
       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
     }
+  end
 end
